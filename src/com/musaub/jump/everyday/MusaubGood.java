42d63fb1cf18fd3e77441ec09df037062e5cab4b
darda <darda@10.0.0.40>
2021-03-31 03:55:32 -0400

    First commit Good Musaub

---
+++ src/com/musaub/jump/everyday/MusaubGood.java
@@ -0,0 +1,17 @@
+package com.musaub.jump.everyday;
+
+public class MusaubGood {
+
+	public static void main(String[] args) {
+		
+		int man = 10;
+		int women = 20;
+		int pepole = man + women;
+		
+		System.out.println("Total pepole :"+ pepole);
+		
+		
+
+	}
+ 
+}
